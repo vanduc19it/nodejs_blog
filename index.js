880ee279220ebd3a7e7c26381f97e4ea77aa592a
vanduc19it <duc19it@gmail.com>
2021-11-22 16:29:54 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/blog/trangchu', (req, res) => {
   res.send('Hello World!')
 })
