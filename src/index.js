df042746bd1506ba4bd0cf79ddd4f6add3fc90b6
vanduc19it <duc19it@gmail.com>
2021-11-23 16:10:56 +0700

    static file &  scss

--- src/index.js
+++ src/index.js
@@ -8,19 +8,18 @@ const port = 3000
 
 app.use(morgan('combined'))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set("views", path.join(__dirname, 'resources/views'))
+
 app.get('/blog/trangchu', (req, res) => {
-  
     res.render('home');
-  
 })
 
 app.get('/blog/news', (req, res) => {
-  
   res.render('news');
-
 })
 
 app.listen(port, () => {
