27e18a2bffa7b9b0b2b3bd5775510d9b7a1c3445
vanduc19it <duc19it@gmail.com>
2021-11-23 15:18:06 +0700

    template engine - handelbars

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const hbs = handlebars.create({ extname: '.hbs'})
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set("views", path.join(__dirname, 'resources/views'))
+app.get('/blog/trangchu', (req, res) => {
+  
+    res.render('home');
+  
+})
+
+app.get('/blog/news', (req, res) => {
+  
+  res.render('news');
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
